5a5a94ba423740aa052308883e55916f98f30c04
sivakesava <sivakesava1@gmail.com>
2021-11-15 13:23:33 -0800

    PowerDNS update 1. prepare.py formatted according to Pylint rules 2. Dockerfile updated with restrictions on 'docutils' version (https://github.com/PowerDNS/pdns/pull/10902)

--- DifferentialTesting/Implementations/Powerdns/prepare.py
+++ DifferentialTesting/Implementations/Powerdns/prepare.py
@@ -1,3 +1,9 @@
+"""
+Script copies the input zone file and the necessary configuration file "bindbackend.conf"
+into an existing or a new PowerDNS container and starts the DNS server on container
+port 53, which is mapped to a host port.
+"""
+
 #!/usr/bin/env python3
 
 import pathlib
@@ -5,28 +11,39 @@ import subprocess
 
 
 def run(zone_file, zone_domain, cname, port, restart, tag):
-
+    """
+    :param zone_file: Path to the Bind-style zone file
+    :param zone_domain: The domain name of the zone
+    :param cname: Container name
+    :param port: The host port which is mapped to the port 53 of the container
+    :param restart: Whether to load the input zone file in a new container
+                        or reuse the existing container
+    :param tag: The image tag to be used if restarting the container
+    """
     if restart:
         subprocess.run(['docker', 'container', 'rm', cname, '-f'],
-                       stdout=subprocess.PIPE)
+                       stdout=subprocess.PIPE, check=True)
         subprocess.run(['docker', 'run', '-dp', str(port)+':53/udp',
-                        '--name=' + cname, 'powerdns' + tag], stdout=subprocess.PIPE)
+                        '--name=' + cname, 'powerdns' + tag], stdout=subprocess.PIPE, check=True)
     else:
-        subprocess.run(
-            ['docker', 'exec', cname, 'pkill', 'pdns_server'], stdout=subprocess.PIPE)
-
-    subprocess.run(['docker', 'cp', zone_file,
-                    cname + ':/usr/local/etc'], stdout=subprocess.PIPE)
-
+        # Kill the running server instance inside the container
+        subprocess.run(['docker', 'exec', cname, 'pkill',
+                        'pdns_server'], stdout=subprocess.PIPE, check=True)
+    # Copy the new zone file into the container
+    subprocess.run(['docker', 'cp', zone_file, cname +
+                    ':/usr/local/etc'], stdout=subprocess.PIPE, check=True)
+    # Create the PowerDNS-specific configuration file
     bindbackend = f'zone "{zone_domain}" {{\n  file "/usr/local/etc/{zone_file.name}";\n  type master;\n}};'
-    with open('bindbackend_'+cname+'.conf', 'w') as f:
-        f.write(bindbackend)
+    with open('bindbackend_'+cname+'.conf', 'w') as file_pointer:
+        file_pointer.write(bindbackend)
+    # Copy the configuration file into the container as "bindbackend.conf"
     subprocess.run(['docker', 'cp', 'bindbackend_'+cname+'.conf',
-                    cname + ':/usr/local/etc/bindbackend.conf'], stdout=subprocess.PIPE)
+                    cname + ':/usr/local/etc/bindbackend.conf'], stdout=subprocess.PIPE, check=True)
     pathlib.Path('bindbackend_'+cname+'.conf').unlink()
-    # `bindbackend.conf` has to be in UNIX style otherwise this error is thrown --
-    # `Caught an exception instantiating a backend: Error in bind configuration '..bindbackend.conf' on line 2: syntax error`.
+    # "bindbackend.conf" has to be in UNIX style otherwise this error is thrown --
+    # Caught an exception instantiating a backend: Error in bind
+    # configuration '..bindbackend.conf' on line 2: syntax error
+    subprocess.run(['docker', 'exec', cname, 'dos2unix',
+                    '/usr/local/etc/bindbackend.conf'], stdout=subprocess.PIPE, check=True)
     subprocess.run(['docker', 'exec', cname,
-                    'dos2unix', '/usr/local/etc/bindbackend.conf'], stdout=subprocess.PIPE)
-    subprocess.run(['docker', 'exec', cname,
-                    'pdns_server', '--daemon'], stdout=subprocess.PIPE)
+                    'pdns_server', '--daemon'], stdout=subprocess.PIPE, check=True)
